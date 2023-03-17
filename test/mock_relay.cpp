559a300e7a6a1ced7c02c88cf3ae7ed2f49d6c31
jcaiMR <jcai@microsoft.com>
2023-03-17 10:29:25 +0000

    fix issue

--- test/mock_relay.cpp
+++ test/mock_relay.cpp
@@ -1008,6 +1008,7 @@ TEST(relay, loop_relay) {
   EXPECT_EQ(vlans_in_loop.size(), 2);
 
   EXPECT_GLOBAL_CALL(event_add, event_add(_, NULL)).Times(5);
+  EXPECT_GLOBAL_CALL(event_base_dispatch, event_base_dispatch(_)).Times(1).WillOnce(Return(-1));
   ASSERT_NO_THROW(loop_relay(vlans_in_loop));
 
   std::async(std::launch::async, [&] () {loop_relay(vlans_in_loop);}).wait_for(std::chrono::milliseconds{1000});
